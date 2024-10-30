f4117598d2e1c25aea4172cd8c3507e239b1dcaa
Alexa13422 <andr.alex04@gmail.com>
2024-10-30 22:02:31 +0100

    message

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,6 +11,7 @@ import java.util.jar.JarFile;
 public class Main {
     public static void main(String[] args) {
         if (args.length < 2) {
+            System.out.println("Use correct typing Main path/to/jar...");
             return;
         }
 
